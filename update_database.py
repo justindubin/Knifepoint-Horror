e4550bd2f37b7df86ba686c6a50af935be15a648
jdubin <justin.dubin@cirrus.com>
2022-02-09 15:05:52 -0600

    alphabetized files and removed readme

--- update_database.py
+++ update_database.py
@@ -1,39 +1,64 @@
 # IMPORTS
 import json
 
-# Decode data from master JSON file
-with open("stories.json", "r") as stories_file:
-    stories = json.load(stories_file)
 
-# Decode data from master JSON characters file
-with open("characters.json", "r") as characters_file:
-    characters = json.load(characters_file)
+# MAIN
+def main():
 
-# Build/update character sheets
-for story in stories:
+    # Decode story data from master JSON file
+    with open("stories.json", "r") as stories_file:
+        stories = json.load(stories_file)
 
-    # Add narrator to characters list if not already there
-    if story["narrator"] not in story["characters"] and story["narrator"] != "":
-        story["characters"].append(story["narrator"])
+    # Decode character data from master JSON file
+    with open("characters.json", "r") as characters_file:
+        characters = json.load(characters_file)
 
-    # Create character entry if none exists
-    logged_characters = [char["name"] for char in characters]
-    for story_character in story["characters"]:
-        if story_character not in logged_characters:
-            new_character_info = {"name": story_character, "age": 0, "summary": "", "mentions": [story["title"]]}
-            characters.append(new_character_info)
+    # Build/update character sheets
+    for story in stories:
 
-    # Update story mentions for each character
-    for character in characters:
-        if character["name"] in story["characters"] and story["title"] not in character["mentions"]:
-            character["mentions"].append(story["title"])
-        elif character["name"] not in story["characters"] and story["title"] in character["mentions"]:
-            character["mentions"].remove(story["title"])
+        # Add narrator to characters list if not already there
+        if story["narrator"] not in story["characters"] and story["narrator"] != "":
+            story["characters"].append(story["narrator"])
 
-# Encode data to master JSON stories file
-with open("stories.json", "w") as stories_file:
-    json.dump(stories, stories_file, indent=4)
+        # Remove duplicate characters in stories file
+        story["characters"] = list(set([char.lower() for char in story["characters"]]))
 
-# Encode data to master JSON characters file
-with open("characters.json", "w") as characters_file:
-    json.dump(characters, characters_file, indent=4)
+        # Alphabetize characters and themes in story data sheet
+        story["characters"].sort()
+        story["themes"].sort()
+
+        # Create character entry if none exists
+        logged_characters = [char["name"].lower() for char in characters]
+        for story_character in story["characters"]:
+            if story_character.lower() not in logged_characters:
+                new_character_info = {
+                    "name": story_character.lower(),
+                    "age": "unknown",
+                    "description": "",
+                    "mentions": [story["title"]]
+                }
+                characters.append(new_character_info)
+
+        # Update story mentions for each character
+        for character in characters:
+
+            if character["name"] in story["characters"] and story["title"] not in character["mentions"]:
+                character["mentions"].append(story["title"])
+            elif character["name"] not in story["characters"] and story["title"] in character["mentions"]:
+                character["mentions"].remove(story["title"])
+
+        # Alphabetize character entries in character data sheet
+        characters = sorted(characters, key=lambda d: d["name"])
+
+    # Encode story data to master JSON file
+    with open("stories.json", "w") as stories_file:
+        json.dump(stories, stories_file, indent=4)
+
+    # Encode character data to master JSON file
+    with open("characters.json", "w") as characters_file:
+        json.dump(characters, characters_file, indent=4)
+
+
+# ENTRY POINT
+if __name__ == '__main__':
+    main()
