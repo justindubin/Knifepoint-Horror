63eeaca131cef3d174b303b04cb4a64a3f83c402
jdubin <justin.dubin@cirrus.com>
2022-02-10 17:04:59 -0600

    time to go

--- update_json_files.py
+++ update_json_files.py
@@ -1,9 +1,18 @@
 # IMPORTS
+import os
 import json
 
 
 # FUNCTIONS
 def load_dump_json(filename: str, action: str, dump_data=None, indent=2):
+
+    # Create file if none exists
+    if not os.path.isfile(filename):
+        print(f"\nNo file '{filename}' exists\n > Creating empty JSON array...")
+        with open(filename, "w") as file_obj:
+            json.dump([], file_obj, indent=indent)
+
+    # Dynamically load/dump data
     if action.lower() == "load":
         with open(filename, "r") as file_obj:
             data_out = json.load(file_obj)
@@ -12,9 +21,13 @@ def load_dump_json(filename: str, action: str, dump_data=None, indent=2):
         with open(filename, "w") as file_obj:
             json.dump(dump_data, file_obj, indent=indent)
     else:
-        raise NameError(f"Unable to handle JSON load/dump request!"
-                        f"\nUser-input '{action}' is not an accepted action input argument."
-                        f"\nPlease ensure 'action' is set to either 'load' or 'dump'.")
+        # Throw name error if user enters invalid action
+        raise NameError(f"Invalid input: '{action}'. Ensure action = 'load' or 'dump'.")
+
+
+def conform_naming(name: str):
+    compatible_name = name.lower().replace(" ", "_").replace(".", "").replace("'", "")
+    return compatible_name
 
 
 # MAIN
@@ -23,6 +36,7 @@ def main():
     # Decode JSON data
     stories = load_dump_json(filename="stories.json", action="load")
     characters = load_dump_json(filename="characters.json", action="load")
+    print("\nJSON data loaded into memory...")
 
     # Build/update character sheets
     all_story_characters = []
@@ -32,8 +46,8 @@ def main():
         if story["narrator"] not in story["characters"] and story["narrator"] != "":
             story["characters"].append(story["narrator"])
 
-        # Remove duplicate characters in stories file
-        story["characters"] = list(set([char.lower() for char in story["characters"]]))
+        # Remove duplicate characters and themes in stories file
+        story["characters"] = list(set([char for char in story["characters"]]))
         story["themes"] = list(set([theme.lower() for theme in story["themes"]]))
 
         # Alphabetize characters and themes in story data sheet
@@ -42,19 +56,35 @@ def main():
 
         # Create character entry if none exists
         logged_characters = [char["name"].lower() for char in characters]
+        character_descriptions = []
         for story_character in story["characters"]:
             all_story_characters.append(story_character)
             if story_character.lower() not in logged_characters:
-                new_character_info = {
-                    "name": story_character.lower(),
+
+                # Create a new character object
+                new_character = {
+                    "name": story_character,
                     "age": "unknown",
                     "description": "",
                     "mentions": [story["title"]]
                 }
-                characters.append(new_character_info)
+                characters.append(new_character)
 
-        # Update story mentions for each character
-        for character in characters:
+            # Create a new character description file if none exists
+            char_fname = conform_naming(story_character)
+            char_desc_dir = f"char_desc/{char_fname}.txt"
+            if not os.path.isfile(char_desc_dir):
+                default_text = f"Replace this text with a description of {story_character}"
+                with open(char_desc_dir, "w") as char_desc:
+                    char_desc.write(default_text)
+
+            # Add description from char_desc file
+            with open(char_desc_dir, "r") as char_desc:
+                character_descriptions.append((story_character, char_desc.read()))
+
+        # Update character information
+        for character in characters:  # ToDo: bug on story iteration
+            character["description"] = [cd[1] for cd in character_descriptions if cd[0] == character["name"]][0]
             if character["name"] in story["characters"] and story["title"] not in character["mentions"]:
                 character["mentions"].append(story["title"])
             elif character["name"] not in story["characters"] and story["title"] in character["mentions"]:
@@ -63,12 +93,30 @@ def main():
         # Alphabetize character entries in character data sheet  ToDo: Also by episode number (link to title)
         characters = sorted(characters, key=lambda d: d["name"])
 
-    # Remove character objects not present in any story object
-    characters = [c for c in characters if c["name"] in all_story_characters]
+    # Remove character objects not present in any story object  ToDo: change comment
+    unmentioned_characters = [c["name"] for c in characters if c["name"] not in all_story_characters]
+    if len(unmentioned_characters) > 0:
+        desc_string = "The following characters don't belong to any story object:"
+        title_string = "! UNMENTIONED CHARACTER(S) ALERT !"
+        print()
+        print("-"*len(desc_string))
+        print(title_string.center(len(desc_string)))
+        print("-"*len(desc_string))
+        print(desc_string)
+        for unmentioned_character in unmentioned_characters:
+            print(f"  + {unmentioned_character}")
+        delete_extras = input("\nDelete unmentioned characters? (y/n): ")
+        if delete_extras.lower() == "y":
+            characters = [c for c in characters if c["name"] not in unmentioned_characters]
+            print(" --- Deleted all unmentioned characters")
+        else:
+            print(" --- No characters deleted")
 
     # Encode JSON data
     load_dump_json(filename="stories.json", action="dump", dump_data=stories, indent=4)
     load_dump_json(filename="characters.json", action="dump", dump_data=characters, indent=4)
+
+    print("\nJSON data files updated successfully...")
 
 
 # ENTRY POINT
